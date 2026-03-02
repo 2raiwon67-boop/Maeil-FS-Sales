905e0e19c418e04cc3771a62b6e162d6d5e390de
2raiwon67-boop <2raiwon67@gmail.com>
2026-03-02 22:51:33 +0900

    feat: 방문일지 TO-DO 대시보드 구조 추가 및 불필요한 AI 챗봇 기능 제거

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * 정적 파일(CSS, JS) 캐시로 초기 로딩 속도 개선
  */
 
-const CACHE_NAME = 'fs-miso-v19';
+const CACHE_NAME = 'fs-miso-v20';
 
 // 캐시할 정적 리소스
 const STATIC_ASSETS = [
@@ -17,7 +17,6 @@ const STATIC_ASSETS = [
     '/icons/icon-192x192.png',
     '/icons/icon-512x512.png',
     '/sw.js',
-    '/js/gemini-chatbot.js',
     '/방문일지.html',
     '/report.html',
     '/proposal.html',
